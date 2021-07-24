382530253acb80a53069cb177b2dcf544d0c97df
Lourenço Gomes <leto.lg@gmail.com>
2021-07-24 10:29:24 +0100

    Initial Commit

---
+++ PlayerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerTest
+//
+//  Created by Lourenço Gomes on 24/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
